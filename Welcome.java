11d21d1c80d8aa00bcc2244712f9d107c23175a2
Kumaresh Gupta <kumaresh_gupta@yahoo.com>
2020-07-30 16:56:34 +0530

    second commit

--- Welcome.java
+++ Welcome.java
@@ -1,5 +1,8 @@
 public class Welcome{
 	public static void main(String [] args){
 		System.out.println("Welcome to Java Git Jenkins Combo");
+		for(int i = 0 ; i<10; i++){
+			System.out.println(i + " - Welcome");
+	}
 }
 }
